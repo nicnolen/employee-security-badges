15a2edd8f9195cbd3b0cedc20fe74e61dfb7783d
Nicholas Nolen <nicnolen@ymail.com>
2022-05-16 15:52:34 -0500

    stored the employees first name, last name, id, and photo url in variables

---
+++ CatWorx.BadgeMaker/Employee.cs
@@ -0,0 +1,11 @@
+// by wrapping the class in CatWorkx.BadgeMaker namespace, you can access it directly from any class that uses CatWorkx.BadgeMaker
+namespace CatWorkx.BadgeMaker
+{
+  class Employee
+  {
+    public string? FirstName;
+    public string? LastName;
+    public int Id;
+    public string? PhotoUrl;
+  }
+}
