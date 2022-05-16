ad7a8fe2c95a8fd7e1ddc77ba1a8d684b474948c
Nicholas Nolen <nicnolen@ymail.com>
2022-05-16 15:30:45 -0500

    added a print-employees method

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -10,20 +10,6 @@ namespace CatWorx.BadgeMaker
 {
   class Program
   {
-    //* Main method serves as the entry point to the application
-    //? static means that the scope of the method is at the class level and can be invoked without creating a new class instance (object). Void means there will be no explicit return type
-    static void Main(string[] args)
-    {
-      // employee-getting code
-      List<string> employees = GetEmployees();
-
-      // loop through all the employees
-      for (int i = 0; i < employees.Count; i++) {
-        // write all the employee names to the console
-        Console.WriteLine(employees[i]);
-      }
-    }
-
     // Method to get employee data from the user
     static List<string> GetEmployees() {
       // Create a new list of employees
@@ -32,7 +18,7 @@ namespace CatWorx.BadgeMaker
       // Collect user values until the value is an empty string
       while (true) {
         // ask the user for an employee name
-        Console.WriteLine("Please enter a name: (leave empty to exit): ");
+        Console.WriteLine("Please enter a name: (ctrl C (Windows) or ⌘C for Mac to exit): ");
 
         // get a name from the console and assign it to a variable
         //! string? sets the variable as a nullable string. ? is a nullable reference type
@@ -48,6 +34,26 @@ namespace CatWorx.BadgeMaker
 
       return employees;
     }
+
+    // method to print employees
+    static void PrintEmployees(List<string> employees) {
+      // loop through all the employees
+      for (int i = 0; i < employees.Count; i++) {
+        // write all the employee names to the console
+        Console.WriteLine(employees[i]);
+      }
+    }
+
+    //* Main method serves as the entry point to the application
+    //? static means that the scope of the method is at the class level and can be invoked without creating a new class instance (object). Void means there will be no explicit return type
+    static void Main(string[] args) {
+      // employee-getting code
+      List<string> employees = GetEmployees();
+      // employee-printing code
+      PrintEmployees(employees);
+    }
   }
 }
+
+
 
