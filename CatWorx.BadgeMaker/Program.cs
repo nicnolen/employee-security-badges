6dd20124a73008134d86a3edba1a2593e0843c0e
Nicholas Nolen <nicnolen@ymail.com>
2022-05-16 14:13:08 -0500

    initial commit

---
+++ CatWorx.BadgeMaker/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//* using imports a common namespace (naming shortcut). Without this naming shortcut, you would have to write System before every Console you use
+//? System contains a collection of commonly used methods, data types and data structures
+using System;
+//? Contains things like lists and dictionaries
+using System.Collections.Generic;
+
+//* Namespace organizes and provides a level of seperation in the code. It is followed by the name of the app
+namespace CatWorx.BadgeMaker
+{
+  class Program
+  {
+    //* Main method serves as the entry point to the application
+    //? static means that the scope of the method is at the class level and can be invoked without creating a new class instance (object). Void means there will be no explicit return type
+    static void Main(string[] args)
+    {
+   
+    }
+  }
+}
